67fcc3265332b6edb88d38b282114dc6ec0a7a4c
Anoushka Goel <125151652+anoushka-10@users.noreply.github.com>
2025-07-28 12:50:11 +0530

    Update ElasticsearchConfig.java

--- Transaction-service/src/main/java/com/paysync/transaction/config/ElasticsearchConfig.java
+++ Transaction-service/src/main/java/com/paysync/transaction/config/ElasticsearchConfig.java
@@ -1,23 +1,23 @@
-package com.paysync.transaction.config;
+// package com.paysync.transaction.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.data.elasticsearch.client.ClientConfiguration;
+// import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+// import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@Configuration
-@EnableElasticsearchRepositories(basePackages = "com.paysync.transaction.repository")
-public class ElasticsearchConfig extends ElasticsearchConfiguration {
-	@Value("${spring.elasticsearch.uris}")
-    private String elasticsearchUris;
+// @Configuration
+// @EnableElasticsearchRepositories(basePackages = "com.paysync.transaction.repository")
+// public class ElasticsearchConfig extends ElasticsearchConfiguration {
+// 	@Value("${spring.elasticsearch.uris}")
+//     private String elasticsearchUris;
 	
 
-    @Override
-    public ClientConfiguration clientConfiguration() {
-        String cleanUri = elasticsearchUris.replace("http://", "");
-        return ClientConfiguration.builder()
-            .connectedTo(cleanUri)
-            .build();
-    }
-}
+//     @Override
+//     public ClientConfiguration clientConfiguration() {
+//         String cleanUri = elasticsearchUris.replace("http://", "");
+//         return ClientConfiguration.builder()
+//             .connectedTo(cleanUri)
+//             .build();
+//     }
+// }
